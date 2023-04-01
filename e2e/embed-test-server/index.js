f470bc2d0e111f05a430580bd9cd130bc4ae8c69
Brendan Arnold <brendan.arnold@policylab.gov.uk>
2023-04-01 11:27:06 +0100

    Some debugging on the embed test server

--- e2e/embed-test-server/index.js
+++ e2e/embed-test-server/index.js
@@ -17,10 +17,13 @@ async function main() {
     const server = http.createServer((req, res) => {
         // If its not the embed.html file, send a 404
         if (!req.url.startsWith('/embed.html')) {
+            console.log('Error 404')
             res.writeHead(404)
             res.end()
             return
         } 
+
+        console.log(req.url)
 
         // Take any URL parameters that start with 'data-' and inject into the template
         const { query } = url.parse(req.url, true)
