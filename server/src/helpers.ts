c6c183b666ce7766023a2e6a2eae7e5d6ba097f9
Brendan Arnold <brendan.arnold@policylab.gov.uk>
2025-11-27 23:19:30 +0000

    Update error handling for proxying

--- server/src/helpers.ts
+++ server/src/helpers.ts
@@ -5733,6 +5733,11 @@ function makeFileFetcher(
     fetch(url, { keepalive: true })
       .then(fsRes => {
 
+        if (!fsRes.ok) {
+          Log.fail(res, 500, "polis_err_finding_file " + path, fsRes.statusText);
+          return
+        }
+
         const whitelistedHeaders = _.pick(fsRes.headers, ['content-encoding', 'cache-control', 'content-type'])
         
         res.set({
@@ -5742,10 +5747,6 @@ function makeFileFetcher(
         
         // Convert Web ReadableStream -> Node stream so we can .pipe()
         let fsReq = Readable.fromWeb(fsRes.body! as unknown as ReadableStream)
-
-        fsReq.on("error", function (err: any) {
-          Log.fail(res, 500, "polis_err_finding_file " + path, err);
-        })
 
         // Substitute the preload data into the file
         if (!_.isUndefined(preloadData)) {
